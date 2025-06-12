052d809bb9857692c3324da62dfeba41e222ef07
Bach Le <bach@bullno1.com>
2025-06-12 23:38:26 +0800

    Add a simple test for async thread pool

---
+++ tests/thread.c
@@ -0,0 +1,20 @@
+#include "common.h"
+#include <bio/bio.h>
+
+static btest_suite_t thread = {
+	.name = "thread",
+	.init_per_test = init_bio,
+	.cleanup_per_test = cleanup_bio,
+};
+
+static void
+async_task(void* userdata) {
+	int* data = userdata;
+	*data = 42;
+}
+
+BIO_TEST(thread, run_async) {
+	int data = 10;
+	bio_run_async_and_wait(async_task, &data);
+	BTEST_EXPECT(data == 42);
+}
