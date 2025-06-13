3154ca49c209f2a3328388e9f85a7a6748853a2e
Bach Le <bach@bullno1.com>
2025-06-13 15:29:36 +0800

    Use stdlib allocator by default if a custom allocator is not provided

--- src/bio.c
+++ src/bio.c
@@ -1,12 +1,27 @@
 #include "internal.h"
+#include <stdlib.h>
 
 bio_ctx_t bio_ctx = { 0 };
 
 const bio_tag_t BIO_PLATFORM_ERROR = BIO_TAG_INIT("bio.error.platform");
 
+static void*
+bio_stdlib_realloc(void* ptr, size_t size, void* ctx) {
+	(void)ctx;
+	if (size == 0) {
+		free(ptr);
+		return NULL;
+	} else {
+		return realloc(ptr, size);
+	}
+}
+
 void
 bio_init(const bio_options_t* options) {
 	bio_ctx.options = *options;
+	if (bio_ctx.options.allocator.realloc == NULL) {
+		bio_ctx.options.allocator.realloc = bio_stdlib_realloc;
+	}
 
 	bio_platform_init();
 	bio_handle_table_init();
--- tests/common.c
+++ tests/common.c
@@ -1,23 +1,9 @@
 #include "common.h"
 #include <bio/bio.h>
-#include <stdlib.h>
-
-static void*
-stdlib_realloc(void* ptr, size_t size, void* ctx) {
-	(void)ctx;
-	if (size == 0) {
-		free(ptr);
-		return NULL;
-	} else {
-		return realloc(ptr, size);
-	}
-}
 
 void
 init_bio(void) {
-	bio_init(&(bio_options_t){
-		.allocator.realloc = stdlib_realloc,
-	});
+	bio_init(&(bio_options_t){ 0 });
 }
 
 void
